17803a51c3beaba1c1753b5b75b1f41b3058e5d0
SandeshLamichhane4473 <slc4473@gmail.com>
2023-04-16 23:22:33 +0545

    My First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { usersRef } from './firebase/Firebase';
+import { addDoc } from 'firebase/firestore'
+import swal from 'sweetalert';
+import { Audio } from 'react-loader-spinner';
 
 function App() {
+  document.write = "Gud Services...";
+
+  const additem = async () => {
+
+    try {
+      console.log("COntrol is here.....")
+      await addDoc(usersRef, { name: "Sandesh Lamichhane" });
+      swal({
+        title: "Good job!",
+        text: "You clicked the button!",
+        icon: "success",
+      });
+    }
+    catch (e) {
+      alert(e);
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className=" bg-red-200 text-9xl  font-bold underline">
+        <button onClick={additem}>Add into firebase</button>
+        <Audio />
+      </h1>
     </div>
   );
 }
---
+++ src/firebase/Firebase.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+    GoogleAuthProvider,
+    getAuth,
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    sendPasswordResetEmail,
+    signOut,
+} from "firebase/auth";
+import {
+    getFirestore,
+    query,
+    getDocs,
+    collection,
+    where,
+    addDoc,
+} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const app = initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEYS,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_API_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_API_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_API_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+});
+
+// Initialize Firebase
+
+const analytics = getAnalytics(app);
+
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const usersRef = collection(db, "users");
